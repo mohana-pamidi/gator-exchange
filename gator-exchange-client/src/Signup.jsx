6ca2f7677636ab11f509887ecc1e5e1bd93365fc
mohana-pamidi <mohanashree.pamidimukkala@gmail.com>
2025-10-13 14:17:48 -0400

    added routing for pages

--- gator-exchange-client/src/Signup.jsx
+++ gator-exchange-client/src/Signup.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
+import { Link } from "react-router-dom";
 
 function Signup()
 {
     return (
         <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
-            <div className="bg-white p-3 style={{ width: '400px' }}">
+            <div className="bg-white p-3 rounded" style={{ width: '400px' }}>
                 <h2>Register</h2>
                 <form>
                 <div className="mb-3">
-                    <label htmlFor="email">
+                    <label htmlFor="Name">
                         <strong>Name</strong>
                     </label>
                     <input
@@ -16,16 +17,45 @@ function Signup()
                         placeholder="Enter Name"
                         autoComplete="off"
                         name="email"
+                        className="form-control rounded-0"
                     />
                 </div>
+
+                <div className="mb-3">
+                    <label htmlFor="Email">
+                        <strong>Eamil</strong>
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="Enter Email"
+                        autoComplete="off"
+                        name="email"
+                        className="form-control rounded-0"
+                    />
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="Password">
+                        <strong>Password</strong>
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="Enter Password"
+                        autoComplete="off"
+                        name="email"
+                        className="form-control rounded-0"
+                    />
+                </div>
+
                 <button type="submit" className="btn btn-success w-100 rounded-0">
                     Register 
                 </button>
-                <p>Already have an account?</p>
-                <button className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
-                    login
-                </button>
                 </form>
+                <p>Already have an account?</p>
+                <Link to="/login" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
+                    login
+                </Link>
+                
             </div>
         </div>
     );
