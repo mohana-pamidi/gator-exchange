8c415f58a9979967f42d2de273ec48e431f6abae
mohana-pamidi <mohanashree.pamidimukkala@gmail.com>
2025-10-14 22:41:49 -0400

    added login page + registration page + connection to database

--- gator-exchange-server/index.js
+++ gator-exchange-server/index.js
@@ -11,6 +11,32 @@ app.use(cors())
 //create database connection
 mongoose.connect("mongodb://127.0.0.1:27017/gator-exchange-users")
 
+app.post("/login", (req, res) => {
+    const {email, password} = req.body;
+
+    usersModel.findOne({email: email})
+
+    //can check for user auth here 
+    .then(user => {
+        if(user)
+        {
+           if(user.password === password)
+            {
+                res.json("Success")
+            } 
+            else
+            {
+                res.json("Wrong Password")
+            } 
+        }
+        else
+        {
+            res.json("No record")
+        }
+        
+    })
+})
+
 //server app
 app.post("/register", (req, res) => {
     //data coming from front end is on req.body
